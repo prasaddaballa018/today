238e8982cee5de0d795a5bd24b72b950ed41e245
unknown <prasaddadalla018@gmail.com>
2022-08-25 15:58:08 +0530

    prasad

---
+++ levelup.py
@@ -0,0 +1,20 @@
+#n1=input("enter 1st num:")
+#n2=input("enter 2nd num:")
+#num1=float(n1)
+#num2=float(n2)
+num1=50
+num2=5
+sum=num1+num2
+print("sum of {} and {} ={}".format(num1,num2,sum))
+sub=num1-num2
+print(sub)
+multi=num1*num2
+print(multi)
+division=num1/num2        #give result in float value  
+print(division)
+power=num1**num2
+print(power)
+floar_dev=num1//num2      #gives result in init
+print(floar_dev)=
+module=num1%num2
+print(module)
---
+++ prasad.py
@@ -0,0 +1,15 @@
+m=int(input("enter marks:"))#40
+if m<=35:
+    print("FAIL")
+else:
+    if m>85 and m<=100:
+        print("ur grade is 'A'")
+     elif m>80 and m<=85:
+        print("ur grade is 'B'")
+     elif m>75 and m<=80:
+        print ("ur grade is 'C'")
+     elif m>34 and m<=75:
+        print("ur grade is 'D'") 
+        
+             
+
